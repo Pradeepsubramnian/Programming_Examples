29750bf00245d101feb6dc9847547815ae19471d
SPradeep <pradeejoy1996@gmail.com>
2023-05-04 07:20:00 +0530

    [Implementation][PY]: basic_variables

---
+++ python_prj/Variables/basic_variables.py
@@ -0,0 +1,8 @@
+#Variables in python
+a = 10
+b = 10
+c = a+b
+print("output: ", a)  #Will print the output
+print(id(a))          #Will print the address
+print(id(b))          #will print the output
+print(type(a))        #Will print the data type
